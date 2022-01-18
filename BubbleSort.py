93c3231dcf6ed65ec482908ff7a6692ab9cfe4ff
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-01-18 12:08:49 +0530

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -2,8 +2,12 @@ arr=[22,44,55,11,33,5]
 print("Original Array :{}".format(arr))
 n=len(arr)
 print("Bubble Sorting -->")
-for i in range(n-1):
-    for j in range(0,n-i-1):
+for i in range(len(arr)-1):
+    swapped=False
+    for j in range(len(arr)-1-i):
         if arr[j]>arr[j+1]:
             arr[j],arr[j+1]=arr[j+1],arr[j]
+            swapped=True
+    if not swapped : #if the array is already sorted , break out the loop
+        break
 print("Sorted Array :{}".format(arr))
