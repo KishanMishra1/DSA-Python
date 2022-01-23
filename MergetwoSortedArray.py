bd8f38e241f2b9ad045f8d25be7a0764679340ee
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-01-24 00:10:21 +0530

    Update MergetwoSortedArray.py

--- MergetwoSortedArray.py
+++ MergetwoSortedArray.py
@@ -10,3 +10,16 @@ def merge(A, B):
         if j<len(B) and i>=len(A):
             A.extend(B[j:])
     return A
+
+def merge_two_sorted_arrays(A: List[int], m: int, B: List[int], n: int) -> None:
+    a, b, write_index = m-1, n-1, m + n - 1
+
+    while b >= 0:
+        if a >= 0 and A[a] > B[b]:
+            A[write_index] = A[a]
+            a -= 1
+        else:
+            A[write_index] = B[b]
+            b -= 1
+
+        write_index -= 1
