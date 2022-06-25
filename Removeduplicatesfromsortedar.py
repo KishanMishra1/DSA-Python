6470783620ee1513d668f1a3dd1b413793ae0304
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-06-25 09:07:12 +0530

    Create Removeduplicatesfromsortedar.py

---
+++ Removeduplicatesfromsortedar.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums)==0:
+            return 0
+        if len(nums)==1:
+            return 1
+        
+        i=0
+        for j in range(1,len(nums)):
+            if nums[j]!=nums[i]:
+                i+=1
+                nums[i]=nums[j]
+            
+        return i+1
+            
+            
+        
