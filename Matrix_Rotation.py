fa912ba3720e944db60c1c80cfd0ac44e0596e54
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-08-27 18:35:57 +0530

    Update Matrix_Rotation.py

--- Matrix_Rotation.py
+++ Matrix_Rotation.py
@@ -22,6 +22,15 @@ def anticlock_rotate(matrix,n):
         n-=1
     return matrix
 
+def display(mat):
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            print(mat[i][j],end=" ")
+        print()
+    print()
 
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
-print(anticlock_rotate(matrix,180))
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+display(matrix)
+clockwise_rotate(matrix,90)
+display(matrix)
+
